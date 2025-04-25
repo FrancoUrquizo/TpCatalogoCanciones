d4c7972e90c3f6718734ebabd0174ed8d57b51c6
MATÍAS RAMIRO BRASBURG <49006634@ort>
2025-04-25 15:15:16 -0300

    Proyecto recien creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP03___Music_StoreBrasburgUrquizo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
